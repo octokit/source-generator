88b1c4009e5a05806cce0760560170f2664bb9bc
Keegan Campbell <me@kfcampbell.com>
2023-01-24 16:11:23 -0800

    Generalize location of post-process file

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export CSHARP_POST_PROCESS_FILE="/home/kfcampbell/github/dev/source-generator/csharp-post-process.sh"
+export CSHARP_POST_PROCESS_FILE="$(pwd)/csharp-post-process.sh"
 
 openapi-generator-cli generate -i schemas/api.github.com/api.github.com.2022-11-28.json -t templates/dotnet -g csharp -o generated/dotnet -p packageName=Octokit --enable-post-process-file
 
