f4505e95e608657d16e9d61032a68101bbfec4bb
Keegan Campbell <me@kfcampbell.com>
2023-03-01 13:54:08 -0800

    First (non-working) pass at Ruby post-processing

--- post-processors/ruby/main.go
+++ post-processors/ruby/main.go
@@ -2,8 +2,116 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 func main() {
-	fmt.Printf("TODO(kfcampbell): implement Ruby post-processor")
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	if len(os.Args) != 2 {
+		return fmt.Errorf("exactly one directory name must be provided to run post-processing on")
+	}
+	dirPath := os.Args[1]
+	log.Printf("running Ruby post-processing on directory %v", dirPath)
+
+	files = make(map[string]fs.FileInfo)
+	err := filepath.Walk(dirPath, walkFiles)
+	if err != nil {
+		return fmt.Errorf("could not read directory %v: %v", dirPath, err)
+	}
+
+	for path, file := range files {
+		fileBytes, err := os.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("could not read file %v: %v", file.Name(), err)
+		}
+		fileContents := string(fileBytes)
+		if len(fileContents) < 1 {
+			// skip empty files that are generated in the Ruby SDK
+			continue
+		}
+
+		fileContents = fixEmptyCaseStatements(fileContents)
+		fileContents = fixThumbsUpThumbsDownProperties(fileContents)
+
+		// TODO(kfcampbell): verify file permission is what we want
+		err = os.WriteFile(path, []byte(fileContents), 0666)
+		if err != nil {
+			return err
+		}
+	}
+
+	// after files are written, initialize a module
+
+	return nil
+}
+
+// TODO(kfcampbell) fix this sleazy global
+var files map[string]fs.FileInfo
+
+func walkFiles(path string, info fs.FileInfo, err error) error {
+	if err != nil {
+		return fmt.Errorf("error walking files: %v", err)
+	}
+
+	// skip directories
+	if info.IsDir() {
+		return nil
+	}
+	files[path] = info
+	return nil
+}
+
+// TODO(kfcampbell): implement and validate
+func fixEmptyCaseStatements(inputFile string) string {
+	/*
+		case mapping_value
+		end
+	*/
+
+	if strings.Contains(inputFile, `case mapping_value`) {
+		caseMappingValue := regexp.MustCompile(`^\s+case mapping_value\s+.*?\s+`)
+		inputFile = caseMappingValue.ReplaceAllString(inputFile, ``)
+	}
+
+	// toReplace := `case mapping_value
+	//                 end`
+
+	// // toReplace := `                        case mapping_value
+	// //                     end`
+
+	// replaceWith := ``
+
+	// if strings.Contains(inputFile, toReplace) {
+	// 	inputFile = strings.ReplaceAll(inputFile, toReplace, replaceWith)
+	// }
+	return inputFile
+}
+
+// TODO(kfcampbell): implement and validate
+func fixThumbsUpThumbsDownProperties(inputFile string) string {
+	/*
+		ONE_REACTIONSPOSTREQUESTBODY_CONTENT ReactionsPostRequestBody_content = iota
+		ONE_REACTIONSPOSTREQUESTBODY_CONTENT
+	*/
+	toReplace := `ONE_REACTIONSPOSTREQUESTBODY_CONTENT ReactionsPostRequestBody_content = iota
+    ONE_REACTIONSPOSTREQUESTBODY_CONTENT`
+
+	replaceWith := `THUMBSUP_REACTIONSPOSTREQUESTBODY_CONTENT ReactionsPostRequestBody_content = iota
+    THUMBSDOWN_REACTIONSPOSTREQUESTBODY_CONTENT`
+
+	if strings.Contains(inputFile, toReplace) {
+		inputFile = strings.ReplaceAll(inputFile, toReplace, replaceWith)
+	}
+
+	return inputFile
 }
