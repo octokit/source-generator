b36dbffafe2da5e75ad81afb96964991656e6764
Keegan Campbell <me@kfcampbell.com>
2023-03-01 13:28:04 -0800

    Revert "Remove generated files from source"
    This reverts commit e97f43405265af454130b4aa940de21f4487badc; these
    files were necessary for the build.

---
+++ generated/csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Identity;
+using Azure.Core;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Authentication.Azure;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+var GHPAT = "TODO";
+
+var authProvider = new AnonymousAuthenticationProvider();
+var requestAdapter = new HttpClientRequestAdapter(authProvider);
+
+
+// Passing path parms
+
+// var pathParameters =new Dictionary<string, object>() {
+//   {"s", "Hey yo!"}
+// };
+// var octocatPathParms = new ApiSdk.Octocat.OctocatRequestBuilder(pathParameters, requestAdapter);
+// Stream requestPathParm = await octocatPathParms.GetAsync();
+
+// StreamReader rPathParms = new StreamReader(requestPathParm);
+// string responsePathParms = rPathParms.ReadToEnd();
+// Console.WriteLine(responsePathParms);
+
+// Passing path parms
+
+// Basic request
+
+var client = new Octokit.ApiClient(requestAdapter);
+var request = await client.Octocat.GetAsync();
+
+StreamReader reader = new StreamReader(request);
+string response = reader.ReadToEnd();
+Console.WriteLine(response);
+
+// Basic request
