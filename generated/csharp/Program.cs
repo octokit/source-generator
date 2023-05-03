ab8beb24f497c59c9a893b4bb9dc33a5898e2feb
Keegan Campbell <me@kfcampbell.com>
2023-05-03 15:19:59 -0700

    Fixups to dotnet demo application

--- generated/csharp/Program.cs
+++ generated/csharp/Program.cs
@@ -3,34 +3,54 @@ using Azure.Core;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.Kiota.Authentication.Azure;
 using Microsoft.Kiota.Http.HttpClientLibrary;
+using Kiota;
 
-var GHPAT = "TODO";
+var token = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+if (string.IsNullOrEmpty(token))
+{
+	Console.WriteLine("GITHUB_TOKEN environment variable is not set");
+	//   throw new Exception("GITHUB_TOKEN environment variable is not set");
+}
 
-var authProvider = new AnonymousAuthenticationProvider();
-var requestAdapter = new HttpClientRequestAdapter(authProvider);
+var authProvider = new ApiKeyAuthenticationProvider(token, "Authorization", ApiKeyAuthenticationProvider.KeyLocation.Header, new string[] { "https://api.github.com" });
+var requestAdapter = new HttpClientRequestAdapter(authProvider, null, null, new HttpClient
+{
+	BaseAddress = new Uri("https://api.github.com/")
+});
+var pathParameters = new Dictionary<string, object>() {
+		{"baseurl", "https://api.github.com/"},
+	};
+var octocatPathParams = new Kiota.Octocat.OctocatRequestBuilder(pathParameters, requestAdapter);
+Action<Kiota.Octocat.OctocatRequestBuilder.OctocatRequestBuilderGetRequestConfiguration> requestConfig =
+	delegate (Kiota.Octocat.OctocatRequestBuilder.OctocatRequestBuilderGetRequestConfiguration config)
+	{
+		config.QueryParameters.S = "Hey yo!";
+		config.Headers.Add("Accept", "application/vnd.github.v3+json");
+	};
 
+try
+{
+	Stream requestPathParam = await octocatPathParams.GetAsync(requestConfig);
+	StreamReader rPathParams = new StreamReader(requestPathParam);
+	string responsePathParams = rPathParams.ReadToEnd();
+	Console.WriteLine(responsePathParams);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+	throw;
+}
 
-// Passing path parms
-
-// var pathParameters =new Dictionary<string, object>() {
-//   {"s", "Hey yo!"}
-// };
-// var octocatPathParms = new ApiSdk.Octocat.OctocatRequestBuilder(pathParameters, requestAdapter);
-// Stream requestPathParm = await octocatPathParms.GetAsync();
-
-// StreamReader rPathParms = new StreamReader(requestPathParm);
-// string responsePathParms = rPathParms.ReadToEnd();
-// Console.WriteLine(responsePathParms);
 
 // Passing path parms
 
 // Basic request
 
-var client = new Octokit.ApiClient(requestAdapter);
-var request = await client.Octocat.GetAsync();
+// var client = new Octokit.ApiClient(requestAdapter);
+// var request = await client.Octocat.GetAsync();
 
-StreamReader reader = new StreamReader(request);
-string response = reader.ReadToEnd();
-Console.WriteLine(response);
+// StreamReader reader = new StreamReader(request);
+// string response = reader.ReadToEnd();
+// Console.WriteLine(response);
 
 // Basic request
