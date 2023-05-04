6d9f41a56ee42c5376e194aae0f120eb6ba63209
Keegan Campbell <me@kfcampbell.com>
2023-05-04 15:16:36 -0700

    Default to descriptions-next directory

--- schemas/main.go
+++ schemas/main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
+var useSchemaNext bool
+
+// Normally I hate using init() but the docs [here](https://pkg.go.dev/flag)
+// recommend it so this usage is safe.
+func init() {
+	flag.BoolVar(&useSchemaNext, "schema-next", true, "Set to false using --schema-next=false to disable using the descriptions-next directory for schema downloads")
+}
+
 func main() {
 	if err := realMain(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("execution finished successfully")
+	log.Println("execution finished successfully")
 }
 
 func realMain() error {
-	fmt.Printf("downloading latest schema version...\n")
+	flag.Parse()
+
 	out, err := os.Create("schemas/downloaded.json")
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
+	logMsg := "Downloading latest schema from descriptions directory"
 	url := "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json"
+	if useSchemaNext {
+		logMsg = "Downloading latest schema from descriptions-next directory"
+		url = "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions-next/api.github.com/api.github.com.json"
+	}
 
+	log.Printf(logMsg)
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
