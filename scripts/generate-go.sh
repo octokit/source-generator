35aa5d5d339250447209a2159e2ac394bd3fb727
Keegan Campbell <me@kfcampbell.com>
2023-10-31 12:53:55 -0700

    Update convenience script

--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Execute generation
-kiota generate -l go --ll trace -o generated/go -n kiota -d schemas/updated/api.github.com.json --co
+# convenience script for local development only
+# .github/workflows/build-go.yml is the source of truth for the generated Go workflow
+# note: before running this script, ensure the generated/go directory has been deleted
+# but the sample app restored:
+# rm -rf generated/go/
+# git restore generated/
 
-# Build and run post-processor
-go build -o $(pwd)post-processors/go/post-processor post-processors/go/main.go
-$(pwd)post-processors/go/post-processor $(pwd)/generated/go
+set -ex
 
-# Build and run the generated code and call the API via generated SDK
+go run schemas/main.go --schema-next=false
+kiota generate -l go --ll trace -o generated/go -n kiota -d schemas/downloaded.json
+go build -o post-processors/go/post-processor post-processors/go/main.go
+post-processors/go/post-processor $(pwd)/generated/go
 cd generated/go
 go build ./...
+go run *.go
