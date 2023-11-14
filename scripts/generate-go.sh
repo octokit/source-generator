bf059e05bb46d3b2c4fcf7266b77dede32558162
Keegan Campbell <me@kfcampbell.com>
2023-11-13 16:20:00 -0800

    Alter generate-go.sh to match go-sdk generation

--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -2,19 +2,14 @@
 
 # convenience script for local development only
 # .github/workflows/build-go.yml is the source of truth for the generated Go workflow
-# note: before running this script, ensure the generated/go directory has been deleted
-# but the sample app restored:
-# rm -rf generated/go/
-# git restore generated/
 
 ./scripts/install-tools.sh
 
 set -ex
 
 go run schemas/main.go --schema-next=false
-kiota generate -l go --ll trace -o generated/go -n kiota -d schemas/downloaded.json
+kiota generate -l go --ll trace -o $(pwd)/../go-sdk -n go-sdk -d schemas/downloaded.json
 go build -o post-processors/go/post-processor post-processors/go/main.go
-post-processors/go/post-processor $(pwd)/generated/go
-cd generated/go
+cd $(pwd)/../go-sdk
+$(pwd)/../source-generator/post-processors/go/post-processor $(pwd)
 go build ./...
-go run *.go
