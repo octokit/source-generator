a191b11589dd239fd723e5940d446f239b64d8ac
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2023-11-17 14:15:55 -0600

    corrects comment about version

--- generated/csharp/GitHub/Middleware/Options/VersionOptions.cs
+++ generated/csharp/GitHub/Middleware/Options/VersionOptions.cs
@@ -8,6 +8,6 @@ public class VersionOptions : IRequestOption
     public string APIVersion { get; set; } = GetAPIVersion();
 
   private static string GetAPIVersion() =>
-    //TODO : Get the version from the assembly
+    //TODO : Get the version from the generator
     "2022-11-28";
 }
