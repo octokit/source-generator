2926a4c1964fa48ebccd634a9e2ef47918ddcdba
Keegan Campbell <me@kfcampbell.com>
2023-12-12 13:33:55 -0800

    Namespace updates: create source directories (#46)
    * Reverse github and octokit directories in go-sdk
    
    * Use pkg directory instead of octokit
    
    * Use src directory for .NET
    
    * Temporarily create update PRs on PR builds
    
    * Undo temporary change

--- scripts/generate-csharp.sh
+++ scripts/generate-csharp.sh
@@ -5,13 +5,13 @@
 go run schemas/main.go --schema-next=false
 
 # Execute generation
-kiota generate -l csharp --ll trace -o generated/csharp/GitHub/Octokit -c OctokitClient -n GitHub.Octokit -d schemas/downloaded.json --co
+kiota generate -l csharp --ll trace -o generated/csharp/src/GitHub -c GitHubClient -n GitHub -d schemas/downloaded.json --co
 
 # Build and run post-processor
 go build -o $(pwd)/post-processors/csharp/post-processor post-processors/csharp/main.go
-$(pwd)/post-processors/csharp/post-processor $(pwd)/generated/csharp/GitHub/Octokit
+$(pwd)/post-processors/csharp/post-processor $(pwd)/generated/csharp/src/GitHub
 
 # Build and run the generated code and call the API via generated SDK
-cd generated/csharp/GitHub
+cd generated/csharp/src
 dotnet restore
 dotnet run --project GitHub.Octokit.csproj
