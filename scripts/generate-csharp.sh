9f82617006591dee6050128dc1de9e1aa3408994
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-01-10 17:25:26 -0600

    Removes the unused part of the generate script - the SDK get's built in the ci step (#47)

--- scripts/generate-csharp.sh
+++ scripts/generate-csharp.sh
@@ -10,8 +10,3 @@ kiota generate -l csharp --ll trace -o generated/csharp/src/GitHub -c GitHubClie
 # Build and run post-processor
 go build -o $(pwd)/post-processors/csharp/post-processor post-processors/csharp/main.go
 $(pwd)/post-processors/csharp/post-processor $(pwd)/generated/csharp/src/GitHub
-
-# Build and run the generated code and call the API via generated SDK
-cd generated/csharp/src
-dotnet restore
-dotnet run --project GitHub.Octokit.csproj
