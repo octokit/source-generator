ded143bcdff648f42f30f78d27e5f412c0c1b174
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-02-08 10:50:41 +0000

    workaround for 4151

--- scripts/generate-java.sh
+++ scripts/generate-java.sh
@@ -5,4 +5,14 @@
 go run schemas/main.go --schema-next=false
 
 # Execute generation
-kiota generate -l java --ll trace -o generated/Java/src/main/java/com/github/api -c GitHubClient -n com.github.api -d schemas/downloaded.json --serializer none --deserializer none --co --ebc
+# The excluded path is a workaround for: https://github.com/microsoft/kiota/issues/4151
+
+kiota generate -l java --ll trace \
+  -o generated/Java/src/main/java/com/github/api \
+  -c GitHubClient \
+  -n com.github.api \
+  -d schemas/downloaded.json \
+  --serializer none \
+  --deserializer none \
+  --exclude-path "/repos/{template_owner}/{template_repo}/generate" \
+  --co --ebc
