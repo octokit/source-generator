a677ec2871263820e96866c2f10d9f99333b0863
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-03-29 08:49:20 -0500

    updates the package update step to use the middleware pattern from our go generator

--- post-processors/csharp/main.go
+++ post-processors/csharp/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -53,6 +56,61 @@ func run() error {
 		if err != nil {
 			return err
 		}
+	}
+
+
+  // cmd := exec.Command("dotnet", "mod", "init", "github.com/octokit/go-sdk")
+	// cmd.Dir = dirPath
+  // var stdout, stderr bytes.Buffer
+	// cmd.Stdout = &stdout
+	// cmd.Stderr = &stderr
+
+  // _ = stdout.String()
+  // stderrOutput := stderr.String()
+
+	// if err != nil {
+	// 	return fmt.Errorf("could not initialize Go module: %v\nfull error log:\n%s", err, stderrOutput)
+	// }
+
+  cmd := exec.Command("kiota", "info", "-l", "csharp", "--json")
+  cmd.Dir = dirPath
+
+  var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+  output, err := cmd.Output()
+  if err != nil {
+    fmt.Printf("could not run kiota info: %v\nfull error log:\n%s", err, stderr.String())
+  }
+
+  // parse the json returned by kiota info, extract the "dependencies" field,
+	// and construct a "dotnet add package" command for each with the "name" and "version" subfields used.
+  // Hoisted from post-processors/go/main.go
+	var infoResult map[string]interface{}
+	if err := json.Unmarshal(output, &infoResult); err != nil {
+		return fmt.Errorf("could not parse kiota info output: %v", err)
+	}
+	deps := infoResult["dependencies"].([]interface{})
+	depsToInstall := make([]string, len(deps))
+	for i, d := range deps {
+		dep := d.(map[string]interface{})
+		name := dep["name"].(string)
+		version := dep["version"].(string)
+
+		fullDep := fmt.Sprintf("%s --version %s", name, version)
+		depsToInstall[i] = fullDep
+	}
+
+	for _, dep := range depsToInstall {
+		cmd = exec.Command("dotnet", "add package", dep)
+		cmd.Dir = dirPath
+
+		_, err := cmd.Output()
+		if err != nil {
+			return fmt.Errorf("could not get dependencies: %v", err)
+		}
+		fmt.Printf("installed dependency %s\n", dep)
 	}
 
 	return nil
