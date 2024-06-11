75e19ea8de94c0a8bc51444e399abea6f3ef2dfd
Keegan Campbell <me@kfcampbell.com>
2024-06-11 11:32:13 -0700

    Modify scripts/generate-dotnet.sh to operate on staged dotnet-sdk

--- scripts/generate-dotnet.sh
+++ scripts/generate-dotnet.sh
@@ -5,10 +5,10 @@
 go run schemas/main.go --schema-next=false
 
 # Execute generation
-kiota generate -l csharp --ll trace -o $(pwd)/../dotnet-sdk/src/GitHub -c GitHubClient -n GitHub -d schemas/downloaded.json --ebc
+kiota generate -l csharp --ll trace -o $(pwd)/stage/dotnet-sdk/src/GitHub -c GitHubClient -n GitHub -d schemas/downloaded.json --ebc
 
 # Build and run post-processor
 go build -o $(pwd)/post-processors/csharp/post-processor post-processors/csharp/main.go
-post-processors/csharp/post-processor $(pwd)/../dotnet-sdk/src/GitHub
-cd ../dotnet-sdk
+post-processors/csharp/post-processor $(pwd)/stage/dotnet-sdk/src/GitHub
+cd stage/dotnet-sdk
 dotnet build
