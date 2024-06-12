a017fec58fd555bb84bc4a64c20f909d477a45dc
Keegan Campbell <me@kfcampbell.com>
2024-06-12 11:53:51 -0700

    Initial commit: schema downloader takes arguments for platform and version

--- schemas/main.go
+++ schemas/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,11 +10,25 @@ import (
 )
 
 var useSchemaNext bool
+var platform string
+var version string
+
+type platformType int
+
+const (
+	dotcom platformType = iota
+	ghes
+	ghec
+)
+
+var allowedPlatforms = []string{"dotcom", "ghes", "ghec"}
 
 // Normally I hate using init() but the docs [here](https://pkg.go.dev/flag)
 // recommend it so this usage is safe.
 func init() {
 	flag.BoolVar(&useSchemaNext, "schema-next", false, "Set to true using --schema-next=true to use the descriptions-next directory for schema downloads")
+	flag.StringVar(&platform, "platform", "dotcom", "Set the platform to download the schema for. Options are dotcom, ghes, ghec")
+	flag.StringVar(&version, "version", "", "Set the version of the schema to download. Required for GHES")
 }
 
 func main() {
@@ -25,6 +40,38 @@ func main() {
 
 func realMain() error {
 	flag.Parse()
+
+	foundPlatform := false
+	for _, p := range allowedPlatforms {
+		if p == platform {
+			foundPlatform = true
+			break
+		}
+	}
+
+	if foundPlatform == false {
+		return fmt.Errorf("invalid platform provided. platform must be one of dotcom, ghes, ghec. given platform: %s", platform)
+	}
+
+	if platform == "ghes" && version == "" {
+		return fmt.Errorf("version is required for GHES platform")
+	}
+
+	if (platform == "dotcom" || platform == "ghec") && version != "" {
+		return fmt.Errorf("version may not be specified for given platform: %s", platform)
+	}
+
+	/*
+		switch platform {
+			case "ghes":
+				break
+			case "ghec":
+				break
+			case "dotcom":
+				break
+			default:
+				return fmt.Errorf("invalid platform provided. platform must be one of dotcom, ghes, ghec. given platform: %s", platform)
+		}*/
 
 	out, err := os.Create("schemas/downloaded.json")
 	if err != nil {
