c1fde5f8f94778a3f9fcf1b9e1906d4eb72cf45d
Keegan Campbell <me@kfcampbell.com>
2024-06-25 13:18:01 -0700

    Schema downloader enterprise support (#64)
    * Initial commit: schema downloader takes arguments for platform and version
    
    * Filename based on platform and version arguments
    
    * downloads the appropriate file for the given platform and version combination
    
    * added platform type enum parsing to get rid of the magic strings and ran file cleanup on 404s
    
    * Refactor to top-level stage directory that includes languages then SDKs
    
    ---------
    
    Co-authored-by: Nick Floyd <nicholas.floyd.info@gmail.com>

--- schemas/main.go
+++ schemas/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,11 +10,37 @@ import (
 )
 
 var useSchemaNext bool
+var platform string
+var version string
 
-// Normally I hate using init() but the docs [here](https://pkg.go.dev/flag)
-// recommend it so this usage is safe.
+type platformType int
+
+const (
+	dotcom platformType = iota
+	ghes
+	ghec
+)
+
+var platformTypeMap = map[string]platformType{
+	"dotcom": dotcom,
+	"ghes":   ghes,
+	"ghec":   ghec,
+}
+
+func (p platformType) String() string {
+	return [...]string{"dotcom", "ghes", "ghec"}[p]
+}
+
+func isValidPlatform(value string) (platformType, bool) {
+	platform, exists := platformTypeMap[value]
+	return platform, exists
+}
+
+// Docs [here](https://pkg.go.dev/flag) recommends this approach.
 func init() {
 	flag.BoolVar(&useSchemaNext, "schema-next", false, "Set to true using --schema-next=true to use the descriptions-next directory for schema downloads")
+	flag.StringVar(&platform, "platform", "dotcom", "Set the platform to download the schema for. Options are dotcom, ghes, ghec")
+	flag.StringVar(&version, "version", "", "Set the version of the schema to download. Required for GHES")
 }
 
 func main() {
@@ -26,26 +53,59 @@ func main() {
 func realMain() error {
 	flag.Parse()
 
-	out, err := os.Create("schemas/downloaded.json")
+	if _, valid := isValidPlatform(platform); valid == false {
+		return fmt.Errorf("invalid platform provided. platform must be one of dotcom, ghes, ghec. given platform: %s", platform)
+	}
+
+	if platform == platformType.String(ghes) && version == "" {
+		return fmt.Errorf("version is required for GHES platform")
+	}
+
+	if (platform == platformType.String(dotcom) || platform == platformType.String(ghec)) && version != "" {
+		return fmt.Errorf("version may not be specified for given platform: %s", platform)
+	}
+
+	fileExt := ".json"
+	fileName := "api.github.com"
+
+	if platform == platformType.String(ghes) {
+		fileName = platform + "-" + version
+	} else if platform == platformType.String(ghec) {
+		fileName = platform
+	}
+
+	downloadedFile := "schemas/" + fileName + fileExt
+
+	out, err := os.Create(downloadedFile)
 	if err != nil {
 		return err
 	}
+
 	defer out.Close()
 
 	logMsg := "Downloading latest schema from descriptions directory"
-	url := "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json"
+	url := "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/" + fileName + "/" + fileName + fileExt
 	if useSchemaNext {
 		logMsg = "Downloading latest schema from descriptions-next directory"
-		url = "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions-next/api.github.com/api.github.com.json"
+		url = "https://raw.githubusercontent.com/github/rest-api-description/main/descriptions-next/" + fileName + "/" + fileName + fileExt
 	}
 
 	log.Printf(logMsg)
 	resp, err := http.Get(url)
+
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode == http.StatusNotFound {
+		// Clean up the created file
+		err := os.Remove(downloadedFile)
+		if err != nil {
+			return fmt.Errorf("Error deleting file: %s. Error: %s", downloadedFile, err)
+		}
+		return fmt.Errorf("Received 404 Not Found for url: %s", url)
+	}
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return err
--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -5,8 +5,8 @@
 set -ex
 
 go run schemas/main.go --schema-next=false
-kiota generate -l go --ll trace -o $(pwd)/stage/go-sdk/pkg/github -n github.com/octokit/go-sdk/pkg/github -d schemas/downloaded.json --ebc
+kiota generate -l go --ll trace -o $(pwd)/stage/go/go-sdk/pkg/github -n github.com/octokit/go-sdk/pkg/github -d schemas/api.github.com.json --ebc
 go build -o post-processors/go/post-processor post-processors/go/main.go
-cd $(pwd)/stage/go-sdk
-$(pwd)/../../post-processors/go/post-processor $(pwd)
+post-processors/go/post-processor $(pwd)/stage/go/go-sdk
+cd $(pwd)/stage/go/go-sdk
 go build ./...
