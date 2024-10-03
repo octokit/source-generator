12de9758eb33a6f9f083cc8b0384339284f1ab14
Nick Floyd <139819+nickfloyd@users.noreply.github.com>
2024-10-03 16:37:03 -0500

    fix: Updates Microsoft.OpenAPI.Kiota generator tool from v1.18.0 to v1.19.0 (#135)

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -6,4 +6,4 @@ go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.59.1
 # Install Kiota
 # This version can only increase, never decrease. If you want to use a lower version, you need to uninstall the tool first.
 # dotnet tool uninstall Microsoft.OpenApi.Kiota --global
-dotnet tool install --global Microsoft.OpenApi.Kiota --version 1.18.0
+dotnet tool install --global Microsoft.OpenApi.Kiota --version 1.19.0
